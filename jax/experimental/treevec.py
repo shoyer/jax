2c51f177b9725c73eb36bd3b17c4e8033a0403a0
Stephan Hoyer <shoyer@google.com>
2021-08-30 10:26:12 -0700

    use _src.dtypes

--- jax/experimental/treevec.py
+++ jax/experimental/treevec.py
@@ -26,13 +26,13 @@ import numpy as np
 import jax.numpy as jnp
 from .. import core
 from .. import api
-from .. import dtypes
 from .. import lax
 from .. import linear_util as lu
 from ..api_util import flatten_fun_nokwargs
 from ..interpreters import batching
 from ..interpreters import partial_eval
 from ..interpreters import xla
+from .._src import dtypes
 from .._src.util import prod, safe_map as map, split_list, unzip2, unzip3
 from ..tree_util import (
     tree_structure, tree_flatten, tree_unflatten,
